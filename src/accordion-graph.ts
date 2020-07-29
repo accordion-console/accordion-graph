fce4406f8a04b43c3e8449d1d0c0b7a89fbe498f
TAEUK KANG <twkang@mantech.co.kr>
2020-07-29 09:40:50 +0900

    edit demo page && set layout

--- src/accordion-graph.ts
+++ src/accordion-graph.ts
@@ -3,30 +3,66 @@ import { html, css, LitElement, PropertyDeclarations } from 'lit-element';
 export class AccordionGraph extends LitElement {  
   static get properties(): PropertyDeclarations {
     return {
-      title: { type: String, },
-      counter: { type: Number, },
     }
-  }
-
-  title = 'Hey there';
-  counter = 5;
-
-  __increment() {
-    this.counter += 1;
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+    <div class="accordion-graph">
+      <div class="toolbar"></div>
+      <div class="graph-or-info">
+        <div class="graph">
+
+        </div>
+
+        <div class="info">
+        
+        </div>
+      </div>
+    </div>
     `;
   }
 
   static styles = css`
     :host {
-      display: block;
-      padding: 25px;
-      color: var(--accordion-graph-text-color, #000);
+      display: flex;
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+    }
+
+    .accordion-graph {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+    }
+
+    .toolbar {
+      flex: 0 0 40px;
+    }
+
+    .graph-or-info {
+      flex: 1 1 auto;
+
+      display: flex;
+      flex-direction: row;
+    }
+
+    .graph {
+      flex: 1 1 auto;
+    }
+
+    .info {
+      flex: 0 0 300px;
+    }
+
+    .accordion-graph,
+    .toolbar,
+    .graph,
+    .info {
+      box-sizing: border-box;
+      border: 1px solid rgb(224, 224, 224);
     }
   `;
 }
